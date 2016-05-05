bfa4c3f1772a38e13461ddb365a602f50fa0e748
Harrison-Metcalfe-SAE <100648@student.sae.edu.au>
2016-05-05 20:24:51 +1000

    Added files via upload
    Initial Commit. Blocks can be placed, but there is not much else to it yet.

---
+++ Assets/Reset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour {
+
+    public void ButtonReset()
+    {
+        Application.LoadLevel("main");
+
+    }
+
+}
---
+++ Assets/cube.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+
+    // Spawn Point for Block Type
+    public GameObject AnchorPoint;
+
+
+    public GameObject PrefabToSpawn;
+
+
+    private GameObject AttachedBuilding = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void ReactToMouseClick()
+    {
+
+        //Is there a building?
+        if(AttachedBuilding != null)
+        {
+            GameObject.Destroy(AttachedBuilding);
+            AttachedBuilding = null;
+        }
+        else
+        {
+            // Spawn the block type
+            AttachedBuilding = GameObject.Instantiate(PrefabToSpawn);
+
+            // Spawn at anchor point
+            AttachedBuilding.transform.position = AnchorPoint.transform.position;
+            AttachedBuilding.transform.SetParent(AnchorPoint.transform);
+        }
+   
+        
+    }
+}
